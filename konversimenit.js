d4af1cfa812fb1b435a469adf1e0d80e01e3becc
inditapryatamap <42919037+inditapryatamap@users.noreply.github.com>
2018-10-07 00:00:31 +0700

    Add files via upload

---
+++ konversimenit.js
@@ -0,0 +1,16 @@
+function konversiMenit(menit) {
+  var seconds = menit%60;
+  var minutes = (menit - seconds)/60;
+  if (String(seconds).length === 1){
+     return minutes+':'+'0'+seconds;
+  } else {
+  return minutes+':'+seconds;
+  }
+
+}
+//output
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:0
